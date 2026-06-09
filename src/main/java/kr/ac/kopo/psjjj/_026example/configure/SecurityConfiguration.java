90deddd8c319d4e3e6519715433e23c2288e96ad
seongjun1011 <sj297916@gmail.com>
2026-06-09 09:51:20 +0900

    Security와 관련된 의존성 추가 접근권한설정, 암호화 설정, 사용자 정보등록 환경설정 파일 클래스를 작성

---
+++ src/main/java/kr/ac/kopo/psjjj/_026example/configure/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package kr.ac.kopo.psjjj._026example.configure;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+// 특정 URI에 접근할수 있는 접근 권한 설정
+    @Bean
+    SecurityFilterChain examMethod01(HttpSecurity http){
+        http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers("/member/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/manager/**").hasRole("MANAGER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+        ).formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+// 암호화 설정
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+// 사용자 정보 등록 설정
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("guest")
+                .password(passwordEncoder().encode("g1234"))
+                .roles("USER")
+                .build();
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password(passwordEncoder().encode("m1234"))
+                .roles("MANAGER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("a1234"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+}
